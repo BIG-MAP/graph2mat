805df5babd6c9ddcc2ee1ae07f279c3b68117f1b
Peter Bjørn Jørgensen <peterbjorgensen@gmail.com>
2023-01-12 15:21:52 +0100

    enable setting logger paths based on environment variables

--- e3nn_matrix/scripts/lit/cli.py
+++ e3nn_matrix/scripts/lit/cli.py
@@ -1,3 +1,4 @@
+import os
 from pytorch_lightning.cli import LightningCLI
 
 class OrbitalMatrixCLI(LightningCLI):
@@ -6,3 +7,23 @@ class OrbitalMatrixCLI(LightningCLI):
         parser.link_arguments("data.basis_files", "model.basis_files")
         parser.link_arguments("data.z_table", "model.z_table")
         parser.link_arguments("data.symmetric_matrix", "model.symmetric_matrix")
+
+        # The following is a hack to set logger defaults based on environment variables
+        init_args = {
+                "save_dir": os.environ.get("PL_LOGGER_SAVE_DIR", "."),
+                "name": os.environ.get("PL_LOGGER_NAME", "lightning_logs"),
+                "version": os.environ.get("PL_LOGGER_VERSION"),
+        }
+
+        if os.environ.get("PL_LOGGER_DEFAULTS_ENABLE") is not None:
+            default_loggers = [{
+                "class_path": "TensorBoardLogger",
+                "init_args": init_args
+                },
+                {
+                "class_path": "CSVLogger",
+                "init_args": init_args
+                },
+            ]
+
+            parser.set_defaults({"trainer.logger": default_loggers})
